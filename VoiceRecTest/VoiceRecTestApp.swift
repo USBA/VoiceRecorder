841418d210d7293f8e674d122ebf7a5fd07b8064
Umayanga Alahakoon <umayanga@usa.com>
2022-07-21 17:21:34 +0530

    Initial Commit

---
+++ VoiceRecTest/VoiceRecTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  VoiceRecTestApp.swift
+//  VoiceRecTest
+//
+//  Created by Umayanga Alahakoon on 2022-07-21.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
